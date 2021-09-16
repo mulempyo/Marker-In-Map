018d15c288aae95191c91b8af90ddb822e163566
codchild <luckyp1@naver.com>
2021-09-16 15:18:42 +0900

    코드 수정

--- app/src/main/java/org/techtown/myapplication67/MapsActivity.kt
+++ app/src/main/java/org/techtown/myapplication67/MapsActivity.kt
@@ -1,5 +1,6 @@
 package org.techtown.myapplication67
 
+import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -29,33 +27,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
          mMap = googleMap
         val LATLNG = LatLng(37.566418,126.977943)
-        var bitmapDrawable: BitmapDrawable
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
-            bitmapDrawable = getDrawable(R.drawable.pngegg) as BitmapDrawable
-        }else{
-            bitmapDrawable = resources.getDrawable(R.drawable.pngegg) as BitmapDrawable
-        }
-
-        val discriptor = BitmapDescriptorFactory.fromBitmap(bitmapDrawable.bitmap)
-        val mark = MarkerOptions()
+        val descriptor = getDescriptorFromDrawable(R.drawable.pngegg)
+        val marker = MarkerOptions()
                 .position(LATLNG)
-                .icon(discriptor)
-        val markerOptions = MarkerOptions().position(LATLNG)
-            .title("Marker in Seoul City Hall")
-        mMap.addMarker(markerOptions)
+                .title("Marker in Seoul")
+                .icon(descriptor)
+        mMap.addMarker(marker)
         val cameraPosition = CameraPosition.Builder()
             .target(LATLNG)
             .zoom(15.0f)
             .build()
         val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
         mMap.moveCamera(cameraUpdate)
-        val marker = MarkerOptions()
-            .position(LATLNG)
-            .title("Seoul City Hall")
-            .snippet("37.566418,126.977943")
-        mMap.addMarker(marker)
+    }
 
-
+    fun getDescriptorFromDrawable(drawableId:Int): BitmapDescriptor {
+        var bitmapDrawable: BitmapDrawable
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
+            bitmapDrawable = getDrawable(drawableId) as BitmapDrawable
+        }else{
+            bitmapDrawable = resources.getDrawable(drawableId) as BitmapDrawable
+        }
+        var scaledBitmap = Bitmap.createScaledBitmap(bitmapDrawable.bitmap,50,50,false)
+      return BitmapDescriptorFactory.fromBitmap(bitmapDrawable.bitmap)
 
     }
 }
